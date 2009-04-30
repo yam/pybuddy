ae7fcbac306a750e20998658eb6ee572350208c9
Laurent Ollagnier <yam@xenbox.fr>
2009-04-30 16:14:37 +0200

    lauch daemon only if script is lauched, else just import module

--- src/pybuddy-daemon.py
+++ src/pybuddy-daemon.py
@@ -321,92 +321,92 @@ def decode_buddy (buddy,msg):
 #######################################
 # MAIN program
 #######################################
+if __name__ == "__main__":
 
-log = logging.getLogger('pybuddy')
+  log = logging.getLogger('pybuddy')
 
 #Default config
-config = RawConfigParser(
-            { 'port': 8888,
-              'address': '127.0.0.1',
-              'user': 'nobody',
-              'loglevel': 'info',
-              'logfile': 'console',
-              'usbproduct': 0002,
-             }
-)
-
-config._sections = {'network':{}, 'system':{}}
-
-config_files = [ "~/.pybuddy.cfg", 
-                 "/etc/pybuddy/pybuddy.cfg", 
-                 "/usr/local/etc/pybuddy.cfg"
-]
-
+  config = RawConfigParser(
+    { 'port': 8888,
+      'address': '127.0.0.1',
+      'user': 'nobody',
+      'loglevel': 'info',
+      'logfile': 'console',
+      'usbproduct': 0002,
+      }
+    )
+  
+  config._sections = {'network':{}, 'system':{}}
+  
+  config_files = [ "~/.pybuddy.cfg", 
+                   "/etc/pybuddy/pybuddy.cfg", 
+                   "/usr/local/etc/pybuddy.cfg"
+                   ]
+  
 #Parse config
-if len(sys.argv) > 1:
+  if len(sys.argv) > 1:
     config_files.append(sys.argv[1])
-    
-config_read = config.read(config_files)
+  
+  config_read = config.read(config_files)
 
-if config.get("system", "logfile") != "console":
+  if config.get("system", "logfile") != "console":
     logging.basicConfig(
-                        filename=config.get("system", "logfile"),
-                        format='%(asctime)s %(levelname)-8s %(message)s',
-    )
-else:
+      filename=config.get("system", "logfile"),
+      format='%(asctime)s %(levelname)-8s %(message)s',
+      )
+  else:
     logging.basicConfig(
-                        stream=sys.stderr,
-                        format='%(asctime)s %(levelname)-8s %(message)s',
-    )
+      stream=sys.stderr,
+      format='%(asctime)s %(levelname)-8s %(message)s',
+      )
 
 
-if config.get("system", "loglevel") == "debug":
+  if config.get("system", "loglevel") == "debug":
     log.setLevel(logging.DEBUG)
-elif config.get("system", "loglevel") == "info":
+  elif config.get("system", "loglevel") == "info":
     log.setLevel(logging.INFO)
 
 
-if config_read:
+  if config_read:
     log.info("Read config file: %s", config_read[0])
-    
+  
 #Initialize device
-log.info("Starting search...")
-try:
+  log.info("Starting search...")
+  try:
     buddy=BuddyDevice(0, int(config.get("system", "usbproduct")))
-except NoBuddyException, e:
+  except NoBuddyException, e:
     log.error("Not found!")
     sys.exit(1)
 
 
 #Daemonize
-log.info("Starting daemon...")
-if os.fork()==0:
+  log.info("Starting daemon...")
+  if os.fork()==0:
     os.setsid()
-else:
+  else:
     sys.exit(0)
 
 #Create server socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((config.get("network", "address"), int(config.get("network", "port"))))
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  s.bind((config.get("network", "address"), int(config.get("network", "port"))))
 
 #Drop privileges
-try:
+  try:
     uid = pwd.getpwnam(config.get("system", "user"))[2]
-except KeyError:
+  except KeyError:
     log.error("Username %s not found, exiting...", config.get("system", "user"))
     sys.exit(1)
-os.setuid(uid)
-
+  os.setuid(uid)
 
 #Main message loop
-while 1:
+  while 1:
     try:
-        message, address = s.recvfrom(8192)
-        log.debug("Got data from %s", address)
-        decode_buddy(buddy, message)
+      message, address = s.recvfrom(8192)
+      log.debug("Got data from %s", address)
+      decode_buddy(buddy, message)
 
     except (KeyboardInterrupt, SystemExit):
-        raise
+      raise
 
-      
+  
